eb8766ebbf8510bd1eb123fbbd86fac28f293cc5
Olga Lokuta <olokuta20j@gmail.com>
2023-12-21 16:31:01 +0400

    changed file structure, started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, {userState, userEffect} from 'react'
+import api from './api'
+
+const App = () => {
+    const [transactions, setTransactions] = userState([])
+    const [formData, setFormData] = useState({
+        
+    })
+}
+
+export default App;
